0ea8ca3210befa66b72301364a34ca95d7ca7773
K BONE <dmozlish182068@bscse.uiu.ac.bd>
2022-05-11 00:27:21 +0600

    implement factory

--- xPart.java
+++ xPart.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+package creational.pattern;
 
 public class xPart {
 	private static Scanner sc;
@@ -24,4 +25,74 @@ public class xPart {
 		}		
 		System.out.println("\n The Sum of All Elements in this Array = " + Sum);
 	}
+}
+
+interface Shape
+{
+    void draw();
+}
+
+
+class Circle implements Shape
+{
+     @Override
+     public void draw()
+     {
+         System.out.println("a circle drawn");
+     }
+}
+
+
+class Square implements Shape
+{
+     @Override
+     public void draw()
+     {
+         System.out.println("a square drawn");
+     }
+}
+
+class Rectangle implements Shape
+{
+     @Override
+     public void draw()
+     {
+         System.out.println("a rectangle drawn");
+     }
+}
+
+
+class ShapeFactory
+{
+ 
+     public Shape getShape(String type) throws Exception
+     {
+         switch (type)
+         {
+             case "Circle":
+                 return new Circle();
+             case "Square":
+                 return new Square();
+             case "Rectangle":
+                 return new Rectangle();
+             default:
+                 throw new Exception( "Shape type : "+type+" cannot be instantiated");
+         }
+     }
+}
+
+class Painter
+{
+     public static void main(String[] args) throws Exception
+     {
+         
+         ShapeFactory shapeFactory = new ShapeFactory();
+         Shape circle=shapeFactory.getShape("Circle");
+         circle.draw();
+         Shape square = shapeFactory.getShape("Square");
+         square.draw();
+         
+         Shape rombus = shapeFactory.getShape("Rombus");
+         rombus.draw();
+     }
 }
