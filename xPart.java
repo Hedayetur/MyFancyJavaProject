cc0a9f1979ceffb7d96c04abb4b2af9427469a6f
Hedayetur <mrahman182065@bscse.uiu,ac.bd>
2022-05-11 23:14:16 +0600

    Change variable name

--- xPart.java
+++ xPart.java
@@ -4,24 +4,24 @@ public class xPart {
 	private static Scanner sc;
 	public static void main(String[] args) 
 	{
-		int Size, i, Sum = 0;
+		int Len, i, S = 0;
 		sc = new Scanner(System.in);
 	 
 		System.out.print(" Please Enter Number of elements in an array : ");
-		Size = sc.nextInt();	
+		Len = sc.nextInt();	
 		
-		int [] a = new int[Size];
+		int [] a = new int[Len];
 		
-		System.out.print(" Please Enter " + Size + " elements of an Array  : ");
-		for (i = 0; i < Size; i++)
+		System.out.print(" Please Enter " + Len + " elements of an Array  : ");
+		for (i = 0; i < Len; i++)
 		{
 			a[i] = sc.nextInt();
 		}   
 
-		for(i = 0; i < Size; i++)
+		for(i = 0; i < Len; i++)
 		{
-			Sum = Sum + a[i]; 
+			S = S + a[i]; 
 		}		
-		System.out.println("\n The Sum of All Elements in this Array = " + Sum);
+		System.out.println("\n The S of All Elements in this Array = " + S);
 	}
 }
